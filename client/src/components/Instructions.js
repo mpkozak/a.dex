c22fd48285dde34964fdeace98c237511b6a35cd
mpkozak <mparkerkozak@gmail.com>
2019-02-04 05:51:49 -0500

    cleaned up instruction typo, added redirect to mobile

--- client/src/components/Instructions.js
+++ client/src/components/Instructions.js
@@ -10,7 +10,7 @@ export default class Instructions extends PureComponent {
           <h4 className="label">Instructions:</h4>
           <ul className="list">
             <li className="step">Select two handheld objects of different colors — markers work well. These will be your controllers.</li>
-            <li className="step">Click on one of the orbs in "Set Colors". Hold one of your trackers up in front of you and click where it appears within the video frame. You should see a tracking circle appear that matched the color and size of your controller. Repeat for the controller.</li>
+            <li className="step">Click on one of the orbs in "Set Colors". Hold one of your controllers up in front of you and click where it appears within the video frame. You should see a tracking circle appear that matches the color of your controller. Repeat for the controller.</li>
             <li className="step">If a tracking circle doesn’t appear (or only appears intermittently), use the "Sensitivity" knob to adjust.</li>
             <li className="step">Volume is controlled by moving the corresponding controller up and down.</li>
             <li className="step">Pitch is controlled by moving the corresponding controller left and right.</li>
--- client/src/index.js
+++ client/src/index.js
@@ -3,4 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+if (
+  navigator.userAgent.match(/Android/i) ||
+  navigator.userAgent.match(/webOS/i) ||
+  navigator.userAgent.match(/iPhone/i) ||
+  navigator.userAgent.match(/iPad/i) ||
+  navigator.userAgent.match(/iPod/i) ||
+  navigator.userAgent.match(/BlackBerry/i) ||
+  navigator.userAgent.match(/Windows Phone/i)
+) {
+  window.location = 'https://kozak.digital/adex-mobile/'
+} else {
+  ReactDOM.render(<App />, document.getElementById('root'));
+};
