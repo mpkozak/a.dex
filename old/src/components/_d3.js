c50bada59907d04744884e22fcd04e5b70d6f002
mpkozak <mparkerkozak@gmail.com>
2020-03-26 18:27:41 -0400

    moving to monorepo

--- old/src/components/_d3.js
+++
@@ -1,5 +0,0 @@
-import { select } from 'd3-selection';
-import { line, curveLinear } from 'd3-shape';
-import { scaleLinear } from 'd3-scale';
-
-export const d3 = { select, line, curveLinear, scaleLinear };
